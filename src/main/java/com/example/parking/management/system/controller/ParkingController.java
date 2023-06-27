9004c0c848519e42d0d5a1877266d37a175505ea
Metodi Hristov <metodi.hristov@documaster.com>
2023-06-27 10:12:19 +0300

    Add the skeleton for parking controler

--- src/main/java/com/example/parking/management/system/controller/ParkingController.java
+++ src/main/java/com/example/parking/management/system/controller/ParkingController.java
@@ -1,13 +1,12 @@
 package com.example.parking.management.system.controller;
 
+import com.example.parking.management.system.model.Vehicle;
 import com.example.parking.management.system.service.ParkingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/parking")
+@RequestMapping("/api/v1/parking")
 public class ParkingController {
 
     private static final String PONG = "pong";
@@ -20,7 +19,30 @@ public class ParkingController {
         this.parkingService = parkingService;
     }
 
-    
+    @GetMapping("/available-spaces")
+    public int getAvailableSpaces() {
+
+        return parkingService.getAvailableSpaces();
+    }
+
+    @GetMapping("/due-amount/{vehicleId}")
+    public double calculateAmount(@PathVariable Long vehicleId) {
+
+        return parkingService.calculateDueAmount(vehicleId);
+    }
+
+    @PostMapping("/register-vehicle")
+    public Long registerVehicle(@RequestBody Vehicle vehicle) {
+
+        return parkingService.registerVehicle(vehicle);
+    }
+
+    @DeleteMapping("/deregister-vehicle/{vehicleId}")
+    public void deregisterVehicle(@PathVariable Long vehicleId) {
+
+        parkingService.deregisterVehicle(vehicleId);
+    }
+
 
     @GetMapping("/ping")
     public String getPing() {
